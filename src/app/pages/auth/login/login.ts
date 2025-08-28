c54dea4dcf827c2f347c3e8442281c6a01bccc14
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-08-28 19:22:24 +0300

    working login is good

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-
+  //saves login and password of user
+ email: string="";
+ password: string="";
+ login() {
+   console.log("Login user data",this.email,this.password);
+ }
 }
